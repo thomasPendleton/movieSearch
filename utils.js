b1490fe0ff0183c5ebdc2b9fe2519e8279fac0b3
thomasPendleton <57029458+thomasPendleton@users.noreply.github.com>
2020-08-20 22:01:06 -0700

    initial commit

---
+++ utils.js
@@ -0,0 +1,38 @@
+
+const debounce = (func, delay) => {
+    let timeoutId;
+    return (...args) => {
+        if(timeoutId){
+            clearTimeout(timeoutId);
+        }
+        timeoutId = setTimeout(() => {
+            func.apply(null, args);
+        }, delay);
+    };
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
